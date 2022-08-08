7cbf7cfedbaa4ff6cd02700e8028bbb6b320d888
dearbeany <980429sb@naver.com>
2022-08-08 22:10:45 +0900

    220808 commit

---
+++ src/sw/_2070_큰놈작은놈같은놈.java
@@ -0,0 +1,31 @@
+package sw;
+
+import java.util.Scanner;
+
+public class _2070_큰놈작은놈같은놈 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int tc = 1; tc <=T; tc++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.print("#" + tc);
+			if(a > b) {
+				System.out.println(" >");
+			}
+			else if(a == b) {
+				System.out.println(" =");
+			}
+			else {
+				System.out.println(" <");
+
+			}
+		}
+	}
+
+}
---
+++ src/sw/_2071_평균값구하기.java
@@ -0,0 +1,26 @@
+package sw;
+
+import java.util.Scanner;
+
+public class _2071_평균값구하기 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int tc = 0; tc<T; tc++) {
+			double sum = 0;
+			for(int i = 0; i < 10; i++) {
+				int n = sc.nextInt();
+				sum += n;
+			}
+			double result = Math.round(sum/10);
+			System.out.println("#" + (tc+1) +" " + (int)(result));
+		}
+		
+
+	}
+
+}
