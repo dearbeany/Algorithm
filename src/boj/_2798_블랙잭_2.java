a4ac70b73001babdbffa5bb50f5f3faaf72e561d
이수빈 <980429sb@naver.com>
2022-08-09 17:58:17 +0900

    220809 commit

--- src/boj/_2798_블랙잭_2.java
+++ src/boj/_2798_블랙잭_2.java
@@ -1,7 +1,33 @@
 package boj;
 
+import java.util.Scanner;
+
 public class _2798_블랙잭_2 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
+		int N = sc.nextInt(); // 카드의 개수
+		int M = sc.nextInt(); // 합
+
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int sum = 0, max = 0;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				for (int k = j + 1; k < N; k++) {
+					sum = arr[i] + arr[j] + arr[k];
+//					System.out.println(i + " " + j + " " + k);
+					if (sum <= M) { // M 이하인 sum 중에서
+						max = Math.max(sum, max); // sum 중 최대의 값
+					}
+				} // end of k
+			} // end of j
+		} // end of i
+		System.out.println(max);
 	}
 }
