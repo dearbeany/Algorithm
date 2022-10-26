0f3eb943454ec8e9fb69ef95cd7ed8c2c54deb87
dearbeany <980429sb@naver.com>
2022-10-26 23:45:45 +0900

    221026 commit

---
+++ src/boj/bronze/_10817_세수.java
@@ -0,0 +1,16 @@
+package boj.bronze;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _10817_세수 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] num = new int[3];
+		for (int i = 0; i < 3; i++) {
+			num[i] = sc.nextInt();
+		}
+		Arrays.sort(num);
+		System.out.println(num[1]);
+	}
+}
