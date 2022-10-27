e8a53925e71a5644a4aa68b1cede76f69e09d61f
dearbeany <980429sb@naver.com>
2022-10-27 23:33:14 +0900

    221027 commit

---
+++ src/boj/bronze/_10818_최소최대.java
@@ -0,0 +1,18 @@
+package boj.bronze;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _10818_최소최대 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		int[] num = new int[n];
+		for (int i = 0; i < n; i++) {
+			num[i] = sc.nextInt();
+		}
+		Arrays.sort(num);
+		System.out.println(num[0] + " " + num[n - 1]);
+	}
+}
